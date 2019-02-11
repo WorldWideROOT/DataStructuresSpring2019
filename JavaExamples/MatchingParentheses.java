c3d665ebaa2121079e1007990426689552f8067e
elenam <elenam@morris.umn.edu>
2019-02-10 19:09:33 -0600

    a couple of comments and reformatting

--- JavaExamples/MatchingParentheses.java
+++ JavaExamples/MatchingParentheses.java
@@ -4,45 +4,43 @@ import java.util.Stack;
 public class MatchingParentheses {
 
 	/**
-	 * A solution for the matching parentheses problem.
-	 * A given string consists of characters (without white spaces) and 
-	 * may have any number of parentheses and curly braces. The program 
-	 * checks if all parentheses and braces form matching 
-	 * non-overlapping pairs. There may be any symbols in-between 
-	 * parentheses and/or braces. 
-         *
-         * For instance, the following two 
-	 * strings are valid sequences: 
-	 * ab(c{}de(fghi))jk
-	 * (a(b)c)d{e(f)}g
-         *
-	 * The following sequences are not valid:
-	 * ab(c{de(fghi))jk (missing a closing curly brace)
-	 * (a(b))c)d{e(f)}g (an extra closing parenthesis after c)
-	 * (a{b)c} (overlapping parentheses and braces)
-	 * }{ (a closing brace before an opening one)
-	 * a(b(c) (missing a closing parenthesis)
-         *
-	 * The program prints the given string and the word "valid"
-	 * or "invalid". If the string is invalid, additionally a brief 
-	 * message is printed indicating what the problem is. For instance,
-	 * when an opening parenthesis doesn't have a closing one, a message 
-	 * might say "Missing a closing parenthesis". 
+	 * A solution for the matching parentheses problem. A given string consists of
+	 * characters (without white spaces) and may have any number of parentheses and
+	 * curly braces. The program checks if all parentheses and braces form matching
+	 * non-overlapping pairs. There may be any symbols in-between parentheses and/or
+	 * braces.
+	 *
+	 * For instance, the following two strings are valid sequences:
+	 * ab(c{}de(fghi))jk (a(b)c)d{e(f)}g
+	 *
+	 * The following sequences are not valid: ab(c{de(fghi))jk (missing a closing
+	 * curly brace) (a(b))c)d{e(f)}g (an extra closing parenthesis after c) (a{b)c}
+	 * (overlapping parentheses and braces) }{ (a closing brace before an opening
+	 * one) a(b(c) (missing a closing parenthesis)
+	 *
+	 * The program prints the given string and the word "valid" or "invalid". If the
+	 * string is invalid, additionally a brief message is printed indicating what
+	 * the problem is. For instance, when an opening parenthesis doesn't have a
+	 * closing one, a message might say "Missing a closing parenthesis".
 	 */
-	
+
 	public static void main(String[] args) {
 		Stack<Character> symbols = new Stack<>();
-		
+
 		// reading input. Assumption: no whitespaces
 		Scanner read = new Scanner(System.in);
 		System.out.println("Please enter your input string");
 		String input = read.next(); // reads until a whitespace or new line
-		
+
 		// checking the input/output
 		System.out.println(input);
-		
-		// this variable might be helpful
+
+		// this variable might be helpful.
+		// Include it as a part of the loop condition and set it to false at
+		// any part of the loop where you discover that the expression is invalid.
 		boolean isValid = true;
+		
+		// Your loop goes here:
 		
 
 	}
